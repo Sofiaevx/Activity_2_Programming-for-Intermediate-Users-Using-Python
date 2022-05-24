5b1e9972a7a39293fd8404192ae658e04f82a090
Sofiaevx <villanueva19.sofia@gmail.com>
2022-05-24 16:06:40 +0800

    Programming for Intermediate Users Using Python Exercise 2.8 Completed

---
+++ Programming for Intermediate Users Using Python/Exercies2_8.py
@@ -0,0 +1,9 @@
+groceries = {"chicken": 8, "apples": 6, "cucumber": 3, "milk": 2, "oranges": 4}
+remove = groceries.pop("oranges")
+print(groceries)
+
+speaker = {"Sir Rafael": 54, "Ms.Joan": 33, "Ms.Dana": 67,}
+print(speaker.keys())
+
+swimming_team_tryout = {"Carl": "Passed", "Quentin": "Failed", "John Y.": "Passed", "Peter": "Failed", "Max T.": "Passed", "Joseph": "Passed", "Jone": "Failed", "Jorge": "Failed", "George": "Passed", "Ben": "Passed", "Jerome": "Passed", "Rick": "Failed", "Max G.": "Failed", "John P.": "Failed", "Vince": "Passed"}
+print(swimming_team_tryout.get("Jorge"))
