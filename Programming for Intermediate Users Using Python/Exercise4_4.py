be489229b3ac9a77173dc0621d4e7f211a933204
Sofiaevx <villanueva19.sofia@gmail.com>
2022-05-24 18:10:09 +0800

    Programming for Intermediate Users Using Python Exercise 4.4 Completed

---
+++ Programming for Intermediate Users Using Python/Exercise4_4.py
@@ -0,0 +1,4 @@
+import functionfile
+
+print(functionfile.sum(2,5))
+print(functionfile.greetings("Sofia"))
---
+++ Programming for Intermediate Users Using Python/functionfile.py
@@ -0,0 +1,8 @@
+def sum(a, b):
+    return a + b
+def product(a, b):
+    return a * b
+def difference(a, b):
+    return a - b
+def greetings(name):
+    print("Hello " + name + "!")
