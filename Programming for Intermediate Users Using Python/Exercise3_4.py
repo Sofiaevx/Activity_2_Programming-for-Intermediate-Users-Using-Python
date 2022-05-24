426dbea09e79f3045dc042e6fa45c6fa223a20b0
Sofiaevx <villanueva19.sofia@gmail.com>
2022-05-24 16:44:06 +0800

    Programming for Intermediate Users Using Python Exercise 3.4 Completed

---
+++ Programming for Intermediate Users Using Python/Exercise3_4.py
@@ -0,0 +1,9 @@
+newfile = open("newfile.txt", "w")
+newfile.write("Python is easy to read, write, and learn.")
+newfile = open("newfile.txt", "a")
+newfile.write("\n Continue to study so that i can use what i learn for my future job")
+newfile.write("\n Create a program")
+newfile.write("\n My goal is to learn more about python and other programming language")
+newfile = open("newfile.txt", "r")
+
+newfile.close()
